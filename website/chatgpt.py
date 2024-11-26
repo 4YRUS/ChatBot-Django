398a9f736170cef02fce401b56dcdcf9dbb584b0
Bipin Sai Surya G <153203221+4YRUS@users.noreply.github.com>
2024-11-26 18:51:57 +0530

    Update chatgpt.py

--- website/chatgpt.py
+++ website/chatgpt.py
@@ -1,20 +1,41 @@
-from openai import OpenAI
+
+
 import os
+import google.generativeai as genai
 
-client = OpenAI(api_key='ENTER YOUR CHAT API KEY HERE')
+genai.configure(api_key="AIzaSyB6kB-C0ommc3IpbIE0OAUsfIKGTGhxO6k")
 
+# Create the model
 def chat(text):
-	completion = client.chat.completions.create(
-		model="gpt-3.5-turbo",
-		messages=[
-		{"role": "user", "content": text}
-		]
+	generation_config = {
+	  "temperature": 1,
+	  "top_p": 0.95,
+	  "top_k": 40,
+	  "max_output_tokens": 8192,
+	  "response_mime_type": "text/plain",
+	}
+
+	model = genai.GenerativeModel(
+	  model_name="gemini-1.5-flash",
+	  generation_config=generation_config,
 	)
 
-	return completion.choices[0].message
+	chat_session = model.start_chat(
+	  history=[
+	  ]
+	)
+
+	response = chat_session.send_message(text)
+	return response.text
+	
+
 
 def send_chat(text):
 	try : 
-		return chat(text)
+		message = chat(text)
+		print(message)
+		return message
 	except:
 		return None
+if __name__=="__main__":
+	send_chat("HELLO GOOGLE HOW ARE YOU ??")
